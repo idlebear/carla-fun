849ec275870d18c489b53171e528f261d6891994
idlebear <idlebear@redmaples.ca>
2021-07-03 15:42:42 -0400

    Update trajectory.py

--- planning/trajectory.py
+++ planning/trajectory.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 - Barry Gilhuly
+# Copyright (C) 2021 - idlebear
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
